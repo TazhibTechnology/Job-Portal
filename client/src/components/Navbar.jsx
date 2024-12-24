82a1ca3ca1df06fffed4596a62a7fae4c85a365a
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-24 15:56:08 +0430

    Check logic in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="shadow py-4">
         <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
             <img src={assets.logo} alt="" />
-            {
+            {/* {
                 user
                 ?<div className="flex items-center gap-3">
                     <Link to={'/applications'}>Applied Jobs</Link>
@@ -20,7 +20,9 @@ const Navbar = () => {
                 <button className="text-gray-600">Recruiter Login</button>
                 <button onClick={e=>openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">Login</button>
             </div>
-            }
+            } */}
+
+            Check
             
         </div>
     </div>
