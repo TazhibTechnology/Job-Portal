77a489202d40466286c3eafd4bb2b09ca003c55b
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-25 20:28:57 +0430

    check app page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -18,7 +18,7 @@ createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> 
     <BrowserRouter>
     <AppContextProvider>
-      <App />
+      {/* <App /> */}
     </AppContextProvider>
   </BrowserRouter>
   </ClerkProvider>
