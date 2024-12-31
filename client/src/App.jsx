a02b56719a8723bd1e562288a81f2217db3ede12
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-31 20:55:18 +0430

    dev Hero Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import ApplyJob from "./pages/ApplyJob"
-import Applications from "./pages/Applications"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
 const App = () => {
   return (
     <div>
       <Routes>
         {/* Home page */}
-        <Route path="/" element={<Home/>}/>
+        <Route path='/' element={<Home />} />
         {/* Apply Job by id */}
-        <Route path="/apply-job/:id" element={<ApplyJob/>}/>
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
         {/* Application Page */}
-        <Route path="/applications" element={<Applications/>}/>
-        
+        <Route path='/applications' element={<Applications />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
