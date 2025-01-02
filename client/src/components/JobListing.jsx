56cd4be26e20b4a5ffe2e95c2f7ec3e1ffb3d18f
TazhibTechology <tazhibtechnology@gmail.com>
2025-01-02 21:34:59 +0430

    dev sidebar job listing

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,33 +1,48 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const JobListing = () => {
-  const { isSearched, searchedFilter } = useContext(AppContext);
+  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
   return (
-    <div>
+    <div className='container wxl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* Sidebar */}
-      <div>
+      <div className='w-full lg:w-1/4 bg-white px-4 '>
         {/* Search Filter from Hero  */}
-        {
-            isSearched && (searchedFilter.title !== "" || searchedFilter.location !== "")&&(
-                <>
-                <h3>Current Search</h3>
-                <div>
-                  {searchedFilter.title &&(
-                    <span>
-
-                    </span>
-                  )}{
-                    searchedFilter.location && (
-                      <span>
-
-                      </span>
-                    )
-                  }
-                </div>
-                </>
-            )
-        }
+        {isSearched &&
+          (searchFilter.title !== "" || searchFilter.location !== "") && (
+            <>
+              <h3 className='font-medium text-lg mb-4 '>Current Search</h3>
+              <div className='mb-4 text-gray-600 '>
+                {searchFilter.title && (
+                  <span className='inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded'>
+                    {searchFilter.title}
+                    <img
+                      onClick={(e) =>
+                        setSearchFilter((prev) => ({ ...prev, title: "" }))
+                      }
+                      className='cursor-pointer'
+                      src={assets.cross_icon}
+                      alt=''
+                    />
+                  </span>
+                )}
+                {searchFilter.location && (
+                  <span className=' ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded'>
+                    {searchFilter.location}
+                    <img
+                      onClick={(e) =>
+                        setSearchFilter((prev) => ({ ...prev, location: "" }))
+                      }
+                      className='cursor-pointer'
+                      src={assets.cross_icon}
+                      alt=''
+                    />
+                  </span>
+                )}
+              </div>
+            </>
+          )}
       </div>
     </div>
   );
