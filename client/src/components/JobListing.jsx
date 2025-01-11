2cc356dd23467c0b6ff5e5715f63f9983943a858
TazhibTechology <tazhibtechnology@gmail.com>
2025-01-11 21:27:03 +0430

    dev jobListing page func

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { assets, JobCategories, JobLocations } from "../assets/assets";
 import JobCard from "./JobCard";
@@ -8,6 +8,54 @@ const JobListing = () => {
     useContext(AppContext);
   const [showFilter, setShowFilter] = useState(false);
   const [CurrentPage, setCurrentPage] = useState(1);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedLocations, setSelectedLocations] = useState([]);
+
+  const [filteredJobs, setFilteredJobs] = useState(jobs);
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategories((prev) =>
+      prev.includes(category)
+        ? prev.filter((c) => c !== category)
+        : [...prev, category]
+    );
+  };
+
+  useEffect(() => {
+    const matchesCategory = (job) =>
+      selectedCategories.length === 0 ||
+      selectedCategories.includes(job.category);
+    const matchesLocation = (job) =>
+      selectedLocations.length === 0 ||
+      selectedLocations.includes(job.location);
+    const matchesTitle = (job) =>
+      searchFilter.title === "" ||
+      job.title.toLowerCase().includes(searchFilter.title.toLowerCase);
+    const matchesSearchLocation = (job) =>
+      searchFilter.location === "" ||
+      job.location.toLowerCase().includes(searchFilter.location.toLowerCase);
+
+    const newFilteredJobs = jobs
+      .slice()
+      .reverse()
+      .filter(
+        (job) =>
+          matchesCategory(job) &&
+          matchesLocation(job) &&
+          matchesTitle(job) &&
+          matchesSearchLocation(job)
+      );
+      setFilteredJobs(newFilteredJobs)
+      setCurrentPage(1)
+  }, [jobs, selectedCategories, selectedLocations, searchFilter]);
+
+  const handleLocationChange = (location) => {
+    setSelectedLocations((prev) =>
+      prev.includes(location)
+        ? prev.filter((c) => c !== location)
+        : [...prev, location]
+    );
+  };
   return (
     <div className='container wxl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* Sidebar */}
@@ -60,7 +108,12 @@ const JobListing = () => {
           <ul className='space-y-4 text-gray-600'>
             {JobCategories.map((category, index) => (
               <li className='flex gap-3 items-center' key={index}>
-                <input className='scale-125' type='checkbox' />
+                <input
+                  className='scale-125'
+                  type='checkbox'
+                  onChange={() => handleCategoryChange(category)}
+                  checked={selectedCategories.includes(category)}
+                />
                 {category}
               </li>
             ))}
@@ -72,7 +125,12 @@ const JobListing = () => {
           <ul className='space-y-4 text-gray-600'>
             {JobLocations.map((location, index) => (
               <li className='flex gap-3 items-center' key={index}>
-                <input className='scale-125' type='checkbox' />
+                <input
+                  className='scale-125'
+                  type='checkbox'
+                  onChange={() => handleLocationChange(location)}
+                  checked={selectedLocations.includes(location)}
+                />
                 {location}
               </li>
             ))}
@@ -86,25 +144,47 @@ const JobListing = () => {
         </h3>
         <p className='mb-8'>Get your desired job from top companies</p>
         <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-          {jobs.slice((CurrentPage-1)*6 , CurrentPage*6).map((job, index) => (
-            <JobCard key={index} job={job} />
-          ))}
+          {filteredJobs.slice((CurrentPage - 1) * 6, CurrentPage * 6)
+            .map((job, index) => (
+              <JobCard key={index} job={job} />
+            ))}
         </div>
         {/* pagination */}
-        {jobs.length > 0 && (
-          <div className="flex items-center justify-center space-x-2 mt-10">
+        {filteredJobs.length > 0 && (
+          <div className='flex items-center justify-center space-x-2 mt-10'>
             <a href='#job-list'>
-              <img onClick={()=>setCurrentPage(Math.max(CurrentPage-1))} src={assets.left_arrow_icon} alt='' />
+              <img
+                onClick={() => setCurrentPage(Math.max(CurrentPage - 1))}
+                src={assets.left_arrow_icon}
+                alt=''
+              />
             </a>
-            {Array.from({ length: Math.ceil(jobs.length / 6) }).map(
+            {Array.from({ length: Math.ceil(filteredJobs.length / 6) }).map(
               (_, index) => (
                 <a key={index} href='#job-list'>
-                  <button onClick={()=>setCurrentPage(index+1)} className={`w-10 h-10 items-center justify-center border border-gray-300 rounded ${CurrentPage ===index+1? 'bg-blue-100 text-blue-500': 'text-gray-500'}`}>{index + 1}</button>
+                  <button
+                    onClick={() => setCurrentPage(index + 1)}
+                    className={`w-10 h-10 items-center justify-center border border-gray-300 rounded ${
+                      CurrentPage === index + 1
+                        ? "bg-blue-100 text-blue-500"
+                        : "text-gray-500"
+                    }`}
+                  >
+                    {index + 1}
+                  </button>
                 </a>
               )
             )}
             <a href='#job-list'>
-              <img onClick={()=>setCurrentPage(Math.min(CurrentPage+1, Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon} alt='' />
+              <img
+                onClick={() =>
+                  setCurrentPage(
+                    Math.min(CurrentPage + 1, Math.ceil(filteredJobs.length / 6))
+                  )
+                }
+                src={assets.right_arrow_icon}
+                alt=''
+              />
             </a>
           </div>
         )}
