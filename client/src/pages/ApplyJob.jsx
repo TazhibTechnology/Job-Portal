b41f4bca374ab66b16d20e41dab56af4d219fc57
TazhibTechology <tazhibtechnology@gmail.com>
2025-01-13 23:40:34 +0430

    dev job portal page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,34 +1,33 @@
-import { useState,useContext, useEffect } from 'react'
-import {  useParams } from 'react-router-dom'
-import {AppContext} from '../context/AppContext'
-import { jobsData } from '../assets/assets'
+import { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { jobsData } from "../assets/assets";
 const ApplyJob = () => {
+  const { id } = useParams();
+  const [JobsData, setJobData] = useState(null);
 
-  const {id} = useParams()
-  const [JobsData, setJobData] = useState(null)
+  const { jobs } = useContext(AppContext);
 
-const {jobs} = useContext(AppContext)
-
-  const fetchJob = async () =>{
-    const data = jobs.filter(job =>job._id === id)
-    if(data.length !== 0){
-      setJobData(data[0])
+  const fetchJob = async () => {
+    const data = jobs.filter((job) => job._id === id);
+    if (data.length !== 0) {
+      setJobData(data[0]);
       console.log(data[0]);
-      
     }
-  }
+  };
 
-  useEffect(()=>{
-    if(jobs.length>0){
-      fetchJob()
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob();
     }
-    
-  },[id, jobs])
+  }, [id, jobs]);
   return JobData ? (
-    <div>ApplyJob</div>): (<div>
-
+    <div></div>
+  ) : (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="w-20 h-20 border-4 border-gray-300 border-t-blue-400 rounded-full animate-spin"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ApplyJob
+export default ApplyJob;
